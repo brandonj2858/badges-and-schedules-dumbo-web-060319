0731c824910994329c3d0b93908ac20f77340fbd
Brandon James <brandonj2858@gmail.com>
2019-05-22 12:47:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,7 @@ end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index
+  attendees.each_with_index do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end
+end
